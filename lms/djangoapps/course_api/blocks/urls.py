cd47fe399561f32ad1db5cc59a34fbb5d32e3b99
mrkost <mr.kostman@gmail.com>
2020-07-11 18:17:28 +0300

    course history endpoint

--- lms/djangoapps/course_api/blocks/urls.py
+++ lms/djangoapps/course_api/blocks/urls.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.conf.urls import url
 
 from .views import BlocksInCourseView, BlocksView
+import lms.djangoapps.course_api.blocks.views_history as history
 
 urlpatterns = [
     # This endpoint requires the usage_key for the starting block.
@@ -19,5 +20,11 @@ urlpatterns = [
         r'^v1/blocks/',
         BlocksInCourseView.as_view(),
         name="blocks_in_course"
+    ),
+
+    url(
+        r'^v1/course_history/',
+        history.BlocksInCourseView.as_view(),
+        name="course_history"
     ),
 ]
